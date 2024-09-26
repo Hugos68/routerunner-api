079f64e5891d8f350aeb0edb147caf840cad08e3
Noud <518707@student.saxion.nl>
2024-09-26 20:17:12 +0200

    changed order table

--- src/database/tables/order.ts
+++ src/database/tables/order.ts
@@ -1,14 +1,23 @@
 import { integer, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 import { address } from "./address";
+import { createInsertSchema } from "drizzle-valibot";
+import { partial } from "valibot";
 
 export const order = pgTable("order", {
-	id: uuid("id").primaryKey().defaultRandom(),
-	quantity: integer("quantity").notNull(),
-	packageType: text("package_type").notNull(),
-	unloadingAddress: uuid("unloading_address")
-		.notNull()
-		.references(() => address.id),
-	unloadingDateTime: timestamp("unloading_date_time").notNull(),
-	deliveryInstructions: text("delivery_instructions").notNull(),
-	status: text("status").notNull(),
+    id: uuid("id").primaryKey().defaultRandom(),
+    quantity: integer("quantity").notNull(),
+    packageType: text("package_type").notNull(),
+    unloadingAddress: uuid("unloading_address")
+        .notNull()
+        .references(() => address.id),
+    unloadingDateTime: timestamp("unloading_date_time").notNull(),
+    deliveryInstructions: text("delivery_instructions").notNull(),
+    status: text("status").notNull(),
 });
+
+
+export type Order = typeof order.$inferSelect;
+
+export const insertOrderSchema = createInsertSchema(order);
+
+export const updateOrderSchema = partial(insertOrderSchema);
