7ca5891cb441ed30afc6cf3c87f3015a58b18b95
Noud <518707@student.saxion.nl>
2024-09-26 21:13:54 +0200

    format

--- src/database/tables/address.ts
+++ src/database/tables/address.ts
@@ -3,13 +3,13 @@ import { createInsertSchema } from "drizzle-valibot";
 import { partial } from "valibot";
 
 export const address = pgTable("address", {
-    id: uuid("id").primaryKey().defaultRandom(),
-    street: text("street").notNull(),
-    number: text("number").notNull(),
-    city: text("city").notNull(),
-    state: text("state").notNull(),
-    country: text("country").notNull(),
-    zip: text("zip").notNull(),
+	id: uuid("id").primaryKey().defaultRandom(),
+	street: text("street").notNull(),
+	number: text("number").notNull(),
+	city: text("city").notNull(),
+	state: text("state").notNull(),
+	country: text("country").notNull(),
+	zip: text("zip").notNull(),
 });
 
 export type Address = typeof address.$inferSelect;
--- src/services/address.ts
+++ src/services/address.ts
@@ -5,28 +5,28 @@ import { database } from "../database/database.js";
 import { type Address, address } from "../database/tables/address.js";
 
 export async function createAddress(data: Omit<Address, "id">) {
-    return await database.insert(address).values(data).returning();
+	return await database.insert(address).values(data).returning();
 }
 
 export async function getAddress(id: Address["id"]) {
-    return await database.select().from(address).where(eq(address.id, id));
+	return await database.select().from(address).where(eq(address.id, id));
 }
 
 export async function getAddresses() {
-    return await database.select().from(address);
+	return await database.select().from(address);
 }
 
 export async function updateAddress(
-    id: Address["id"],
-    data: Partial<Omit<Address, "id">>,
+	id: Address["id"],
+	data: Partial<Omit<Address, "id">>,
 ) {
-    return await database
-        .update(address)
-        .set(data)
-        .where(eq(address.id, id))
-        .returning();
+	return await database
+		.update(address)
+		.set(data)
+		.where(eq(address.id, id))
+		.returning();
 }
 
 export async function deleteAddress(id: Address["id"]) {
-    return await database.delete(address).where(eq(address.id, id)).returning();
+	return await database.delete(address).where(eq(address.id, id)).returning();
 }
