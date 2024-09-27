2b8b98b8c550b0c51e63643d2060b612a247b88f
Noud <518707@student.saxion.nl>
2024-09-27 15:18:00 +0200

    .

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,5 +1,5 @@
-export const ARGON2ID_CONFIGURATION: Parameters<typeof Bun.password.hash>[1] = {
-	algorithm: "argon2id",
-	memoryCost: 19_456,
-	timeCost: 2,
-};
+export const HASH_CONFIG: Parameters<typeof Bun.password.hash>[1] = {
+    algorithm: "argon2id",
+    memoryCost: 19_456,
+    timeCost: 2,	
+} 
