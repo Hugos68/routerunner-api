0e9ff19fd3ac9fd7b9aa0408981aa6e984236a7c
Noud <518707@student.saxion.nl>
2024-09-27 15:23:44 +0200

    added password hashiing

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -18,6 +18,7 @@ const safeColumns = (() => {
 
 export const createUser = async (input: unknown) => {
 	const values = parse(CreateUserSchema, input);
+	values.password = await Bun.password.hash(values.password, HASH_CONFIG);
 	const [result] = await database.insert(users).values(values).returning();
 	return result;
 };
@@ -40,25 +41,20 @@ export const getUser = async (id: User["id"]) => {
 
 export const updateUser = async (id: User["id"], input: unknown) => {
 	const values = parse(UpdateUserSchema, input);
-
 	if ("password" in values && values.password !== undefined) {
-		const hashedPassword = await Bun.password.hash(
+		values.password = await Bun.password.hash(
 			values.password,
 			HASH_CONFIG,
 		);
-		values.password = hashedPassword;
 	}
-
 	const [user] = await database
 		.update(users)
 		.set(values)
 		.where(eq(users.id, id))
 		.returning(safeColumns);
-
 	if (user === undefined) {
 		throw new NotFoundError("User not found");
 	}
-
 	return user;
 };
 
@@ -72,108 +68,3 @@ export const deleteUser = async (id: User["id"]) => {
 	}
 	return user;
 };
-
-// export async function createUser(data: Omit<User, "id">) {
-// 	try {
-// 		const userObject = parse(createInsertSchema(users), data);
-// 		const result = await database
-// 			.insert(users)
-// 			.values({
-// 				...userObject,
-// 				password: await Bun.password.hash(userObject.password, {
-// 					algorithm: "argon2id",
-// 					memoryCost: 19_456,
-// 					timeCost: 2,
-// 				}),
-// 			})
-// 			.returning();
-// 		return Result.ok(result);
-// 	} catch (error) {
-// 		if (error instanceof ValiError) {
-// 			return Result.err("Validation", error.message);
-// 		}
-// 		if (error instanceof Error) {
-// 			return Result.err("Unknown", error.message);
-// 		}
-// 		return Result.err("Unknown", "Something went wrong");
-// 	}
-// }
-
-// export async function getUser(id: User["id"]) {
-// 	try {
-// 		const { password: _, ...columns } = getTableColumns(users);
-// 		const result = await database
-// 			.select(columns)
-// 			.from(users)
-// 			.where(eq(users.id, id));
-
-// 		if (result.length === 0) {
-// 			throw new Error("User not found");
-// 		}
-
-// 		return Result.ok(result);
-// 	} catch (error) {
-// 		if (error instanceof Error) {
-// 			return Result.err("Unknown", error.message);
-// 		}
-// 		return Result.err("Unknown", "Something went wrong");
-// 	}
-// }
-
-// export async function getUsers() {
-// 	try {
-// 		const { password: _, ...columns } = getTableColumns(users);
-// 		const result = await database.select(columns).from(users);
-// 		return Result.ok(result);
-// 	} catch (error) {
-// 		if (error instanceof Error) {
-// 			return Result.err("Unknown", error.message);
-// 		}
-// 		return Result.err("Unknown", "Something went wrong");
-// 	}
-// }
-
-// export async function updateUser(
-// 	id: User["id"],
-// 	data: Partial<Omit<User, "id">>,
-// ) {
-// 	try {
-// 		const result = await database
-// 			.update(users)
-// 			.set({
-// 				...(data.password
-// 					? {
-// 							...data,
-// 							password: await Bun.password.hash(data.password, {
-// 								algorithm: "argon2id",
-// 								memoryCost: 19_456,
-// 								timeCost: 2,
-// 							}),
-// 						}
-// 					: data),
-// 			})
-// 			.where(eq(users.id, id))
-// 			.returning();
-// 		return Result.ok(result);
-// 	} catch (error) {
-// 		if (error instanceof Error) {
-// 			return Result.err("Unknown", error.message);
-// 		}
-// 		return Result.err("Unknown", "Something went wrong");
-// 	}
-// }
-
-// export async function deleteUser(id: User["id"]) {
-// 	try {
-// 		const result = await database
-// 			.delete(users)
-// 			.where(eq(users.id, id))
-// 			.returning();
-// 		return Result.ok(result);
-// 	} catch (error) {
-// 		if (error instanceof Error) {
-// 			return Result.err("Unknown", error.message);
-// 		}
-// 		return Result.err("Unknown", "Something went wrong");
-// 	}
-// }
