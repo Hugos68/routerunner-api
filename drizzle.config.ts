c4e434fe73c65dab26d542e13232caebfe193c9b
hugos68 <hugokorteapple@gmail.com>
2024-10-14 20:18:10 +0200

    Massively simplified environment variable setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,13 +3,8 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
 	dialect: "postgresql",
 	schema: "./src/database/tables/*.ts",
-	schemaFilter: "public",
+
 	dbCredentials: {
-		host: process.env.POSTGRES_HOST as string,
-		port: Number.parseInt(process.env.POSTGRES_PORT as string),
-		database: process.env.POSTGRES_DATABASE as string,
-		user: process.env.POSTGRES_USERNAME as string,
-		password: process.env.POSTGRES_PASSWORD as string,
-		ssl: (process.env.POSTGRES_SSL as string) === "true",
+		url: String(process.env.DATABASE_URL),
 	},
 });
