926de8585305a8dae890bc8addf23bda9d7715d7
Noud <518707@student.saxion.nl>
2024-10-17 16:35:53 +0200

    fixed hashing test

--- test/hashing.test.ts
+++ test/hashing.test.ts
@@ -1,10 +1,11 @@
 import { describe, expect, it } from "bun:test";
-import { hash } from "../src/database/columns/hash.ts";
+import { HASH_CONFIG } from "../src/utility/constants.ts";
+
 describe("Hashing Tests", () => {
 	it("should hash a password", () => {
 		const password = "1234567890";
-		const hashedPassword = hash(password);
+		const hashedPassword = Bun.password.hashSync(password, HASH_CONFIG);
 		expect(hashedPassword).toBeDefined();
-		expect(hashedPassword).not.toBe(password);
+		expect(Bun.password.verify(password, hashedPassword)).toBeTruthy();
 	});
 });
