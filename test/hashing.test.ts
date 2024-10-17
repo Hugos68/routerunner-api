5a20bbaa005dd4bd684254305ecd9858ba4920dc
Noud <518707@student.saxion.nl>
2024-10-17 17:35:46 +0200

    fixed test

--- test/hashing.test.ts
+++ test/hashing.test.ts
@@ -2,10 +2,15 @@ import { describe, expect, it } from "bun:test";
 import { HASH_CONFIG } from "../src/utility/constants.ts";
 
 describe("Hashing Tests", () => {
-	it("should hash a password", () => {
+	it("should hash a password", async () => {
 		const password = "1234567890";
+		const wrongPassword = "0123456789";
 		const hashedPassword = Bun.password.hashSync(password, HASH_CONFIG);
 		expect(hashedPassword).toBeDefined();
-		expect(Bun.password.verify("0123456789", hashedPassword)).toBeFalse();
+		const passwordMatches = await Bun.password.verify(
+			wrongPassword,
+			hashedPassword,
+		);
+		expect(passwordMatches).toBe(false);
 	});
 });
