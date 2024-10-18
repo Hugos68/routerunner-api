8bd4a8618c83c94983521f1350ce408bc04e6371
hugos68 <hugokorteapple@gmail.com>
2024-10-18 13:56:21 +0200

    comments

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -2,6 +2,7 @@ import type { CookieOptions } from "hono/utils/cookie";
 
 /**
  * Hash configuration following the OWASP recommendation.
+ *
  * @see https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
  */
 export const HASH_CONFIG = {
@@ -12,11 +13,19 @@ export const HASH_CONFIG = {
 
 export const SESSION_COOKIE_KEY = "session_id";
 
+/**
+ * Cookie configuration following the OWASP recommendation.
+ *
+ * @see https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
+ */
 export const SESSION_COOKIE_CONFIG = {
 	sameSite: "strict",
 	secure: true,
 	httpOnly: true,
 } satisfies CookieOptions;
 
-// 30 days
+/**
+ * Time after which a session expires.
+ * Time: 30 days in ms.
+ */
 export const SESSION_LIFETIME = 30 * 24 * 60 * 60 * 1000;
